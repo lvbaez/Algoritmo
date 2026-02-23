3f54fda225739ade9d6b89576fcb79715d0034af
lvbaez <lizbaezl@gmail.com>
2026-02-23 12:43:53 -0300

    Area de triangulo

--- Clase01/Clase01_Ejercicio1.py
+++ Clase01/Clase01_Ejercicio1.py
@@ -1,1 +1,11 @@
-#Primer ejercicio
+# Calcular el área de un triángulo conocidos sus tres lados
+
+a = float(input("Ingrese el lado a: "))
+b = float(input("Ingrese el lado b: "))
+c = float(input("Ingrese el lado c: "))
+
+s = (a + b + c) / 2 #fórmula de semiperímetro
+
+area = (s * (s - a) * (s - b) * (s - c)) ** 0.5 #fórmula de área
+
+print("El área del triángulo es:", area)
